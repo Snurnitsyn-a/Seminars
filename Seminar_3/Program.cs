f85f114cbc7c0840a43952af187a84fbdda356bd
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-04-01 11:08:09 +0300

    Task 1 solved

---
+++ Seminar_3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача No18. Общее обсуждение
+Напишите программу, которая по заданному номеру четверти, показывает
+ диапазон возможных координат точек в этой четверти (x и y).*/
+Console.WriteLine("Enter the number of the quarter");
+int Q = int.Parse(Console.ReadLine());
+string result = GetXY(Q);
+Console.WriteLine(result);
+string GetXY(int Q)
+{
+    if (Q == 1)
+    {
+        return "x > 0 && y > 0";
+    }
+    if (Q == 2)
+    {
+        return "x < 0 && y > 0";
+    }
+    if (Q == 3)
+    {
+        return "x < 0 && y < 0";
+    }
+    if (Q == 4)
+    {
+        return "x > 0 && y < 0";
+    }
+    return "Invalid data has been inserted";
+}
