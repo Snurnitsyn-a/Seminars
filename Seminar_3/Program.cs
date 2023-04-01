a20e2ed422634c19cc3a95f6d04cb8d01643430b
Anton Snurnitsyn <anton.snu@yahoo.com>
2023-04-01 22:33:03 +0300

    Task 18, 21, 22

--- Seminar_3/Program.cs
+++ Seminar_3/Program.cs
@@ -56,4 +56,11 @@ double GetDistance(int x1, int x2, int y1, int y2)
 от 1 до N.
 ● 5 -> 1, 4, 9, 16, 25.
 ● 2 -> 1,4*/
+
 int n = int.Parse(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    Console.Write(i *i + ", "); 
+    i++;
+}
